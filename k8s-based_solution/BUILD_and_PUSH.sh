e282d9b73aa02699a695f69dca96c4370a1bdb1a
Oleksandr Nezhynskyi <oleksandr.nezhynskyi@gmail.com>
2022-01-25 12:39:31 +0100

    WIP

--- k8s-based_solution/BUILD_and_PUSH.sh
+++ k8s-based_solution/BUILD_and_PUSH.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# Prerequisites for Build App
+brew install leiningen
+brew install openjdk
+
 # Download Application from GitHub
 # URL: https://github.com/ThoughtWorksInc/infra-problem/archive/refs/heads/master.zip 
 # Build App, create Docker Images and push into ECR
 echo Download Application from GitHub
+mkdir ../application
 curl https://github.com/ThoughtWorksInc/infra-problem/archive/refs/heads/master.zip -o ../application/master.zip -L 
 cd ../application
 unzip ../application/master.zip -d .
@@ -16,35 +21,41 @@ make clean all
 echo -------------------------------------------------------
 echo Build Docker images with Application components
 # Copy JAR files into app_images directory
-mv ./build/front-end.jar ../k8s-based_solution/app_images/front-end_service/
-mv ./build/newsfeed.jar ../k8s-based_solution/app_images/newsfeed_service/
-mv ./build/quotes.jar ../k8s-based_solution/app_images/quotes_service/
-mv ./public/* ../k8s-based_solution/app_images/static_assets/
+cp ./build/front-end.jar ../k8s-based_solution/app_images/front-end_service/
+cp ./build/newsfeed.jar ../k8s-based_solution/app_images/newsfeed_service/
+cp ./build/quotes.jar ../k8s-based_solution/app_images/quotes_service/
+cp -R ./front-end/public ../k8s-based_solution/app_images/static_assets/
 cd ..
 rm -rf application
+
 echo Autenticate in ECR
 # Authenticate in Private ECR registry
 # URL of ECR need to be replaces from Terrafrom output
 # aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 776120585128.dkr.ecr.eu-central-1.amazonaws.com
 
 # Building Docker image for FrontendService
+cd k8s-based_solution/app_images/
 echo Build 'frontend' Image
-cd ../k8s-based_solution/app_images/front-end_service/
+cd front-end_service/
 docker build -t frontend-service .
+rm *.jar
 
 # Building Docker image for newsfeedService
 echo Build 'newsfeed' Image
-cd ../k8s-based_solution/app_images/newsfeed_service/
+cd ../newsfeed_service/
 docker build -t newsfeed-service .
+rm *.jar
 
 # Building Docker image for quotesService
 echo Build 'quotes' Image
-cd ../k8s-based_solution/app_images/quotes_service/
+cd ../quotes_service/
 docker build -t quotes-service .
+rm *.jar
 
 echo Build 'static assests' Image
-cd ../k8s-based_solution/app_images/static_assests/
+cd ../static_assests/
 docker build -t static-assets-service .
+rm -rf 
 
 echo -------------------------------------------------------
 echo Building Images DONE! 
