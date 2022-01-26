cf13aa52a3d735700381bf9f4706244bf6b1b81f
Oleksandr Nezhynskyi <oleksandr.nezhynskyi@gmail.com>
2022-01-26 22:15:45 +0100

    Uncomment TF

--- k8s-based_solution/infrastructure/application_backend/main.tf
+++ k8s-based_solution/infrastructure/application_backend/main.tf
@@ -107,139 +107,137 @@ resource "aws_ecr_repository" "static-service" {
   )
 }
 
+## Create an EKS cluster for run Application
+### Create EKS
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 
-# ## Create an EKS cluster for run Application
-# ### Create EKS
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_id
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_id
-# }
+data "aws_availability_zones" "available" {
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   load_config_file       = false
-# }
+locals {
+  cluster_name   = "tw-eks-test-cluster"
+  instance_types = "t3.medium"
+  k8s_version    = "1.21"
+}
 
-# data "aws_availability_zones" "available" {
-# }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-# locals {
-#   cluster_name   = "tw-eks-test-cluster"
-#   instance_types = "t3.medium"
-#   k8s_version    = "1.21"
-# }
+  name                 = "tw-app-k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+  public_subnet_tags = merge(
+    var.additional_tags, {
+      "Component"                                   = "Shared"
+      "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+      "kubernetes.io/role/elb"                      = "1"
+    },
+  )
 
-#   name                 = "tw-app-k8s-vpc"
-#   cidr                 = "172.16.0.0/16"
-#   azs                  = data.aws_availability_zones.available.names
-#   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-#   public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
-#   enable_nat_gateway   = true
-#   single_nat_gateway   = true
-#   enable_dns_hostnames = true
+  private_subnet_tags = merge(
+    var.additional_tags, {
+      "Component"                                   = "Shared"
+      "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+      "kubernetes.io/role/internal-elb"             = "1"
+    },
+  )
+}
 
-#   public_subnet_tags = merge(
-#     var.additional_tags, {
-#       "Component"                                   = "Shared"
-#       "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#       "kubernetes.io/role/elb"                      = "1"
-#     },
-#   )
+resource "aws_security_group" "additional" {
+  name        = "SG Additional for EKS"
+  description = "Allow traffic"
+  vpc_id      = module.vpc.vpc_id
 
-#   private_subnet_tags = merge(
-#     var.additional_tags, {
-#       "Component"                                   = "Shared"
-#       "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#       "kubernetes.io/role/internal-elb"             = "1"
-#     },
-#   )
-# }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-# resource "aws_security_group" "additional" {
-#   name        = "SG Additional for EKS"
-#   description = "Allow traffic"
-#   vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 
-#   ingress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
-# }
+  cluster_name                    = local.cluster_name
+  cluster_version                 = local.k8s_version
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-#   cluster_name                    = local.cluster_name
-#   cluster_version                 = local.k8s_version
-#   cluster_endpoint_private_access = true
-#   cluster_endpoint_public_access  = true
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
 
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 20
+    instance_types         = [local.instance_types]
+    vpc_security_group_ids = [aws_security_group.additional.id]
+  }
 
-#   cluster_addons = {
-#     coredns = {
-#       resolve_conflicts = "OVERWRITE"
-#     }
-#     kube-proxy = {}
-#     vpc-cni = {
-#       resolve_conflicts = "OVERWRITE"
-#     }
-#   }
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-#   eks_managed_node_group_defaults = {
-#     ami_type               = "AL2_x86_64"
-#     disk_size              = 20
-#     instance_types         = [local.instance_types]
-#     vpc_security_group_ids = [aws_security_group.additional.id]
-#   }
-
-#   eks_managed_node_groups = {
-#     blue = {}
-#     green = {
-#       min_size     = 1
-#       max_size     = 2
-#       desired_size = 1
-
-#       instance_types = [local.instance_types]
-#       capacity_type  = "SPOT"
-#       labels = merge(
-#         var.additional_tags, {
-#           "Component" = "Shared"
-#         },
-#       )
-#       taints = {
-#         dedicated = {
-#           key    = "dedicated"
-#           value  = "gpuGroup"
-#           effect = "NO_SCHEDULE"
-#         }
-#       }
-#       tags = merge(
-#         var.additional_tags, {
-#           "Component" = "Shared"
-#         },
-#       )
-#     }
-#   }
-# }
+      instance_types = [local.instance_types]
+      capacity_type  = "SPOT"
+      labels = merge(
+        var.additional_tags, {
+          "Component" = "Shared"
+        },
+      )
+      taints = {
+        dedicated = {
+          key    = "dedicated"
+          value  = "gpuGroup"
+          effect = "NO_SCHEDULE"
+        }
+      }
+      tags = merge(
+        var.additional_tags, {
+          "Component" = "Shared"
+        },
+      )
+    }
+  }
+}
